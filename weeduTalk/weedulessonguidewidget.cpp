1fd94af40fd6507465ef4b0bbda74202969db088
sss3000 <jianghaiying@kibey.com>
2018-05-23 19:32:58 +0800

    背景透明

--- weeduTalk/weedulessonguidewidget.cpp
+++ weeduTalk/weedulessonguidewidget.cpp
@@ -1,12 +1,16 @@
 #include "weedulessonguidewidget.h"
 #include "ui_weedulessonguidewidget.h"
 #include "utility/downloadutil.h"
+#include <QPainter>
 
 WeEduLessonGuideWidget::WeEduLessonGuideWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::WeEduLessonGuideWidget)
 {
     ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowMinimizeButtonHint );
+    setAttribute(Qt::WA_TranslucentBackground, true);
+
 }
 
 WeEduLessonGuideWidget::~WeEduLessonGuideWidget()
@@ -17,6 +21,27 @@ WeEduLessonGuideWidget::~WeEduLessonGuideWidget()
 void WeEduLessonGuideWidget::setLessonInfoAndType(const wetalkgetLessonInfo _lessonInfo, const LESSON_WEEDUSCHOOL_TYPE _lessonType)
 {
     DownloadUtil::downloadImage(this, _lessonInfo.picture_url, ui->lessonCover,
-                                "QLabel#labelPic", false );
+                                "QLabel#lessonCover", false );
+
+}
+
+void WeEduLessonGuideWidget::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.fillRect(this->rect(), QColor(0, 0, 0, 80));
+}
+
+void WeEduLessonGuideWidget::on_listenButton_clicked()
+{
+
+}
+
+void WeEduLessonGuideWidget::on_readButton_clicked()
+{
+
+}
+
+void WeEduLessonGuideWidget::on_quizButton_clicked()
+{
 
 }
--- weeduTalk/weedulessonguidewidget.h
+++ weeduTalk/weedulessonguidewidget.h
@@ -17,6 +17,15 @@ public:
     ~WeEduLessonGuideWidget();
     void setLessonInfoAndType (const wetalkgetLessonInfo _lessonInfo, const LESSON_WEEDUSCHOOL_TYPE _lessonType);
 
+protected:
+    void paintEvent(QPaintEvent *event);
+private slots:
+    void on_listenButton_clicked();
+
+    void on_readButton_clicked();
+
+    void on_quizButton_clicked();
+
 private:
     Ui::WeEduLessonGuideWidget *ui;
 };
